b7f52dc3f29aa8d04ad3f1c59dc9d24f296aa4b2
Leonildo Muniz <89280268+LeonildoMuniz@users.noreply.github.com>
2022-02-17 21:54:14 -0300

    Aula-17-02-22

---
+++ CadastroFriend/CadastroFriend/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace CadastroFriend
+{
+    partial class frmCadastro
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnLocaliza = new System.Windows.Forms.Button();
+            this.dgvPessoa = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtNascimento = new System.Windows.Forms.TextBox();
+            this.txtCelular = new System.Windows.Forms.TextBox();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.lblCelular = new System.Windows.Forms.Label();
+            this.lblCidade = new System.Windows.Forms.Label();
+            this.lblData = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.s = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPessoa)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.ForeColor = System.Drawing.Color.DimGray;
+            this.txtNome.Location = new System.Drawing.Point(257, 17);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(497, 28);
+            this.txtNome.TabIndex = 1;
+            // 
+            // btnLocaliza
+            // 
+            this.btnLocaliza.BackColor = System.Drawing.Color.SlateGray;
+            this.btnLocaliza.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLocaliza.ForeColor = System.Drawing.Color.White;
+            this.btnLocaliza.Location = new System.Drawing.Point(21, 16);
+            this.btnLocaliza.Name = "btnLocaliza";
+            this.btnLocaliza.Size = new System.Drawing.Size(90, 30);
+            this.btnLocaliza.TabIndex = 5;
+            this.btnLocaliza.Text = "Localizar";
+            this.btnLocaliza.UseVisualStyleBackColor = false;
+            this.btnLocaliza.Click += new System.EventHandler(this.btnLocaliza_Click);
+            // 
+            // dgvPessoa
+            // 
+            this.dgvPessoa.AllowUserToAddRows = false;
+            this.dgvPessoa.AllowUserToDeleteRows = false;
+            this.dgvPessoa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPessoa.Location = new System.Drawing.Point(12, 332);
+            this.dgvPessoa.Name = "dgvPessoa";
+            this.dgvPessoa.ReadOnly = true;
+            this.dgvPessoa.Size = new System.Drawing.Size(775, 281);
+            this.dgvPessoa.TabIndex = 11;
+            this.dgvPessoa.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPessoa_CellClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtNascimento);
+            this.groupBox1.Controls.Add(this.txtCelular);
+            this.groupBox1.Controls.Add(this.txtCidade);
+            this.groupBox1.Controls.Add(this.lblCelular);
+            this.groupBox1.Controls.Add(this.lblCidade);
+            this.groupBox1.Controls.Add(this.lblData);
+            this.groupBox1.Controls.Add(this.txtNome);
+            this.groupBox1.Controls.Add(this.lblNome);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.DimGray;
+            this.groupBox1.Location = new System.Drawing.Point(12, 73);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 180);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cadastro Friend";
+            // 
+            // txtNascimento
+            // 
+            this.txtNascimento.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNascimento.ForeColor = System.Drawing.Color.DimGray;
+            this.txtNascimento.Location = new System.Drawing.Point(257, 54);
+            this.txtNascimento.Name = "txtNascimento";
+            this.txtNascimento.Size = new System.Drawing.Size(497, 28);
+            this.txtNascimento.TabIndex = 2;
+            // 
+            // txtCelular
+            // 
+            this.txtCelular.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCelular.ForeColor = System.Drawing.Color.DimGray;
+            this.txtCelular.Location = new System.Drawing.Point(257, 141);
+            this.txtCelular.Name = "txtCelular";
+            this.txtCelular.Size = new System.Drawing.Size(497, 28);
+            this.txtCelular.TabIndex = 4;
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCidade.ForeColor = System.Drawing.Color.DimGray;
+            this.txtCidade.Location = new System.Drawing.Point(257, 94);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(497, 28);
+            this.txtCidade.TabIndex = 3;
+            // 
+            // lblCelular
+            // 
+            this.lblCelular.AutoSize = true;
+            this.lblCelular.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCelular.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblCelular.Location = new System.Drawing.Point(17, 148);
+            this.lblCelular.Name = "lblCelular";
+            this.lblCelular.Size = new System.Drawing.Size(63, 21);
+            this.lblCelular.TabIndex = 10;
+            this.lblCelular.Text = "Celular";
+            // 
+            // lblCidade
+            // 
+            this.lblCidade.AutoSize = true;
+            this.lblCidade.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCidade.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblCidade.Location = new System.Drawing.Point(17, 101);
+            this.lblCidade.Name = "lblCidade";
+            this.lblCidade.Size = new System.Drawing.Size(63, 21);
+            this.lblCidade.TabIndex = 9;
+            this.lblCidade.Text = "Cidade";
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblData.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblData.Location = new System.Drawing.Point(17, 61);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(166, 21);
+            this.lblData.TabIndex = 8;
+            this.lblData.Text = "Data de Nascimento";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblNome.Location = new System.Drawing.Point(17, 24);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(57, 21);
+            this.lblNome.TabIndex = 7;
+            this.lblNome.Text = "Nome";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnFechar);
+            this.groupBox2.Controls.Add(this.btnLimpar);
+            this.groupBox2.Controls.Add(this.btnExcluir);
+            this.groupBox2.Controls.Add(this.btnEditar);
+            this.groupBox2.Controls.Add(this.btnCadastrar);
+            this.groupBox2.Controls.Add(this.btnLocaliza);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.Color.DimGray;
+            this.groupBox2.Location = new System.Drawing.Point(12, 266);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(775, 60);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Opções";
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.BackColor = System.Drawing.Color.SlateGray;
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.ForeColor = System.Drawing.Color.White;
+            this.btnFechar.Location = new System.Drawing.Point(664, 16);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(90, 30);
+            this.btnFechar.TabIndex = 10;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = false;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.SlateGray;
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.ForeColor = System.Drawing.Color.White;
+            this.btnLimpar.Location = new System.Drawing.Point(544, 16);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(90, 30);
+            this.btnLimpar.TabIndex = 9;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.BackColor = System.Drawing.Color.SlateGray;
+            this.btnExcluir.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.ForeColor = System.Drawing.Color.White;
+            this.btnExcluir.Location = new System.Drawing.Point(408, 16);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(90, 30);
+            this.btnExcluir.TabIndex = 8;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = false;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.SlateGray;
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.ForeColor = System.Drawing.Color.White;
+            this.btnEditar.Location = new System.Drawing.Point(276, 16);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(90, 30);
+            this.btnEditar.TabIndex = 7;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.BackColor = System.Drawing.Color.SlateGray;
+            this.btnCadastrar.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.ForeColor = System.Drawing.Color.White;
+            this.btnCadastrar.Location = new System.Drawing.Point(150, 16);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(90, 30);
+            this.btnCadastrar.TabIndex = 6;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = false;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // s
+            // 
+            this.s.BackColor = System.Drawing.Color.SlateGray;
+            this.s.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.s.ForeColor = System.Drawing.Color.White;
+            this.s.Location = new System.Drawing.Point(13, 12);
+            this.s.Name = "s";
+            this.s.Size = new System.Drawing.Size(775, 50);
+            this.s.TabIndex = 9;
+            this.s.Text = "Cadastro de Amigos";
+            this.s.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmCadastro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 625);
+            this.Controls.Add(this.s);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvPessoa);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmCadastro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmCadastro_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPessoa)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnLocaliza;
+        private System.Windows.Forms.DataGridView dgvPessoa;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtCelular;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.Label lblCelular;
+        private System.Windows.Forms.Label lblCidade;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Label s;
+        private System.Windows.Forms.TextBox txtNascimento;
+    }
+}
+
---
+++ CadastroFriend/CadastroFriend/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace CadastroFriend
+{
+    public partial class frmCadastro : Form
+    {
+
+        #region String Conexão
+            SqlConnection conexao = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\CadastroFriend\CadastroFriend\DBAmigos.mdf;Integrated Security=True");
+        #endregion
+
+        public frmCadastro()
+        {
+            InitializeComponent();
+        }
+
+        #region Fechar
+            private void btnFechar_Click(object sender, EventArgs e)
+            {
+                this.Close();
+            }
+        #endregion
+
+        #region Carrega DGV
+            public void CarregaDGV()
+            {
+
+                try
+                {
+                    string str = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\CadastroFriend\CadastroFriend\DBAmigos.mdf;Integrated Security=True";
+                    string query = "SELECT * FROM Pessoa";
+                    SqlConnection conexao = new SqlConnection(str);
+                    SqlCommand cmd = new SqlCommand(query, conexao);
+                    conexao.Open();
+                    cmd.CommandType = CommandType.Text;
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable pessoa = new DataTable();
+                    da.Fill(pessoa);
+
+                    dgvPessoa.DataSource = pessoa;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro:" + ex);
+                }
+                finally
+                {
+
+                    conexao.Close();
+                }
+
+
+             }
+
+
+            private void frmCadastro_Load(object sender, EventArgs e)
+            {
+                CarregaDGV();
+            }
+
+
+        #endregion
+
+        #region Localizar
+            private void btnLocaliza_Click(object sender, EventArgs e)
+            {
+                try
+                {
+                    conexao.Open();
+                    SqlCommand cmd = new SqlCommand("Localizar", conexao);
+                    cmd.Parameters.AddWithValue("@nome", SqlDbType.NChar).Value = txtNome.Text.Trim();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    if (dr.Read())
+                    {
+                        txtNascimento.Text = dr["nascimento"].ToString();
+                        txtNome.Text = dr["nome"].ToString();
+                        txtCelular.Text = dr["celular"].ToString();
+                        txtCidade.Text = dr["cidade"].ToString();
+                        MessageBox.Show("Dados encontrados com sucesso!", "Pesquisa localizada");
+                        conexao.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Não foi encontrado nenhum resultado!", "Sem resultado de pesquisa");
+                    }
+                }
+
+                finally
+                {
+                    conexao.Close();
+                }
+
+
+            }
+        #endregion
+
+        #region Cadastrar
+
+            private void btnCadastrar_Click(object sender, EventArgs e)
+            {
+            try
+            {
+                conexao.Open();
+                SqlCommand cmd = new SqlCommand("Inserir", conexao);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@nome", SqlDbType.NChar).Value = txtNome.Text.Trim();
+                cmd.Parameters.AddWithValue("@nascimento", SqlDbType.NChar).Value = txtNascimento.Text.Trim();
+                cmd.Parameters.AddWithValue("@cidade", SqlDbType.NChar).Value = txtCidade.Text.Trim();
+                cmd.Parameters.AddWithValue("@celular", SqlDbType.NChar).Value = txtCelular.Text.Trim();
+                cmd.ExecuteNonQuery();
+                CarregaDGV();
+                MessageBox.Show("Pessoa Cadastrada com sucesso!", "Cadastro de Pessoas");
+                txtNome.Text = "";
+                txtNascimento.Text = "";
+                txtCelular.Text = "";
+                txtCidade.Text = "";
+                txtNome.Focus();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+
+
+        #endregion
+
+        #region Editar
+            private void btnEditar_Click(object sender, EventArgs e)
+            {
+            try
+            {
+                conexao.Open();
+                SqlCommand cmd = new SqlCommand("Atualizar", conexao);
+                cmd.Parameters.AddWithValue("@nome", SqlDbType.NChar).Value = txtNome.Text.Trim();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@nascimento", SqlDbType.NChar).Value = txtNascimento.Text.Trim();
+                cmd.Parameters.AddWithValue("@cidade", SqlDbType.NChar).Value = txtCidade.Text.Trim();
+                cmd.Parameters.AddWithValue("@celular", SqlDbType.NChar).Value = txtCelular.Text.Trim();
+                cmd.ExecuteNonQuery();
+                CarregaDGV();
+                MessageBox.Show("Pessoa atualizada com sucesso!", "Atualização de Pessoas");
+                txtNome.Text = "";
+                txtNascimento.Text = "";
+                txtCidade.Text = "";
+                txtCelular.Text = "";
+                txtNome.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        #endregion
+
+        #region Excluir
+            private void btnExcluir_Click(object sender, EventArgs e)
+            {
+                try
+                {
+                    conexao.Open();
+                    SqlCommand cmd = new SqlCommand("Excluir", conexao);
+                    cmd.Parameters.AddWithValue("@nome", SqlDbType.NChar).Value = txtNome.Text.Trim();
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.ExecuteNonQuery();
+                    CarregaDGV();
+                    MessageBox.Show("Pessoa Excluida com sucesso!", "Exclusão de Pessoas");
+                    txtNome.Text = "";
+                    txtNascimento.Text = "";
+                    txtCidade.Text = "";
+                    txtCelular.Text = "";
+                    txtNome.Focus();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    conexao.Close();
+                }
+        }
+
+
+
+        #endregion
+
+        #region Limpar
+
+            private void btnLimpar_Click(object sender, EventArgs e)
+            {
+                txtNome.Text = "";
+                txtNascimento.Text = "";
+                txtCidade.Text = "";
+                txtCelular.Text = "";
+                txtNome.Focus();
+            }
+
+
+
+
+        #endregion
+
+        #region Seleciona DGV
+            private void dgvPessoa_CellClick(object sender, DataGridViewCellEventArgs e)
+            {
+                if (e.RowIndex >= 0)
+                {
+                    DataGridViewRow row = this.dgvPessoa.Rows[e.RowIndex];
+
+                    txtNome.Text = row.Cells[0].Value.ToString();
+                    txtNascimento.Text = row.Cells[1].Value.ToString();
+                    txtCidade.Text = row.Cells[2].Value.ToString();
+                    txtCelular.Text = row.Cells[3].Value.ToString();
+
+                }
+            }
+
+        #endregion
+
+    }
+}
